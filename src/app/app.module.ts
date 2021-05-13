6d92c1c7a83c4d33653943280614c01a8ec80a53
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-13 22:11:01 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScopesComponent } from './scopes/scopes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScopesComponent
   ],
   imports: [
     BrowserModule,
